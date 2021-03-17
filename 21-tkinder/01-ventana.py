f878d73ede44fe73dea5a6c4dbac47c410b278f7
anderalvarez <anderalvarez15@gmail.com>
2021-03-17 20:17:15 +0100

    commit

--- 21-tkinder/01-ventana.py
+++ 21-tkinder/01-ventana.py
@@ -1,19 +1,43 @@
 from tkinter import *
 
-# Crear ventana raiz
-ventana = Tk()
+class Programa:
+    def __init__(self):
+        self.title = "Titulo de la ventana"
+        self.icon = "./21-tkinder/imagenes/logotipo.ico"
+        self.size = "1000x600"
+        self.resizable = True
 
-# Parametros de tamano por defecto
-ventana.geometry("1000x600")
+    def cargar(self):
+        # Crear ventana raiz
+        ventana = Tk()
+        self.ventana = ventana
 
-# Bloquear el tamano de la ventana
-ventana.resizable(0,0) # Anchura y altura (1,0) deja dimensionar el ancho pero no el alto
+        # Parametros de tamano por defecto
+        ventana.geometry(self.size)
 
-# Icono de la ventana (Tiene que ser .ico)
-ventana.iconbitmap("./21-tkinder/imagenes/logotipo.ico")
+        # Bloquear el tamano de la ventana
+        if self.resizable == True:
+            ventana.resizable(1,1) # Anchura y altura (1,0) deja dimensionar el ancho pero no el alto
+        else:
+            ventana.resizable(0,0)
+        # Icono de la ventana (Tiene que ser .ico)
+        ventana.iconbitmap(self.icon)
 
-# Titulo de la ventana
-ventana.title("ESTE ES EL TITULO DE LA VENTANA")
-# Arrancar y mostrar la ventana hasta que se cierre
-ventana.mainloop()
+        # Titulo de la ventana
+        ventana.title(self.title)
 
+    def addTexto(self, dato):
+        texto = Label(self.ventana, text=dato)
+        texto.pack()
+    
+    def mostar(self):
+        # Arrancar y mostrar la ventana hasta que se cierre
+        self.ventana.mainloop()
+
+
+# Instanciar mi programa
+programa = Programa()
+programa.cargar()
+programa.addTexto("Texto añadido desde funcion")
+programa.addTexto("Viva España")
+programa.mostar()
