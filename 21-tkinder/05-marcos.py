362eb6840bcb2ee4e45797f02ef9beb95be46c2b
anderalvarez <anderalvarez15@gmail.com>
2021-03-19 11:24:39 +0100

    vs

---
+++ 21-tkinder/05-marcos.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+ventana = Tk()
+
+ventana.title("Marcos")
+ventana.geometry("500x500")
+
+# Le especifico la ventana donde va a estar y su tamaño
+marco = Frame(ventana, width=200, height=200)
+
+# Como Tk, tiene un .config
+marco.config(
+    bg="red",
+    bd=5,
+    relief="solid"
+)
+marco.pack(side=RIGHT, anchor=NE)
+
+marco = Frame(ventana, width=200, height=200)
+marco.config(
+    bg="green",
+    bd=5,
+    relief="solid"
+)
+marco.pack(side=RIGHT, anchor=SE)
+
+ventana.mainloop()
